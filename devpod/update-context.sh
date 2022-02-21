f6679abb1b5be3b1a977a3897c2dc78f8483e7d2
Yair Dar <yair.dar@outlook.com>
2022-02-22 01:27:43 +0200

    nu

--- devpod/update-context.sh
+++ devpod/update-context.sh
@@ -1,6 +1,9 @@
 #/usr/bin/env bash
+# require 'realpath tools.shtool'
+_ME_PARENT=$(dirname $(readlink -f $0))
 
 # install/update devpod dir
+export DEVPOD_INIT_DIR=.
 curl -L \
     "https://raw.githubusercontent.com/yairdar/devpod/main/init.sh" \
 | bash
