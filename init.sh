910aa47bae3412a0c64a90dc32941cb3ec200451
Yair Dar <yair.dar@outlook.com>
2022-02-22 00:34:53 +0200

    update

--- init.sh
+++ init.sh
@@ -4,7 +4,8 @@ mkdir -p devpod && cd devpod
 BASE_FILE_URL=https://raw.githubusercontent.com/yairdar/devpod/main/src/devpod
 xargs -P 4 -I{} wget $BASE_FILE_URL/{} -O {} << FHJ
 Taskfile.yml
-setup.all.sh
+install-deps.sh
 setup.os.tools.sh
 setup.zsh.tools.yml
+update-context.sh
 FHJ
