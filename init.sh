0609ccb5ba825300da8d5df01e22a1648eb25b16
Yair Dar <yair.dar@outlook.com>
2022-02-22 00:16:35 +0200

    add release

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -e
-BASE_FILE_URL=https://raw.githubusercontent.com/yairdar/devpod/main/src/devpod
 mkdir -p devpod && cd devpod      
+BASE_FILE_URL=https://raw.githubusercontent.com/yairdar/devpod/main/src/devpod
 xargs -P 4 -I{} wget $BASE_FILE_URL/{} -O {} << FHJ
 Taskfile.yml
 setup.all.sh
