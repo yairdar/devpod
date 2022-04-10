4093f9d2f5905442c6d53de39013ba74ecd67c47
yairdardev <yairvdar@gmail.com>
2022-04-10 16:50:24 +0300

    ready

--- init.sh
+++ init.sh
@@ -5,12 +5,12 @@ mkdir -p $DEVPOD_INIT_DIR && cd $DEVPOD_INIT_DIR
 
 BASE_FILE_URL=https://raw.githubusercontent.com/yairdar/devpod/main/src/devpod
 xargs -P 4 -I{} wget $BASE_FILE_URL/{} -O {} << MRK_XRGS
+Taskfile.yml
 install-deps.sh
 setup.cloud.tools.yml
 setup.os.docker.yaml
 setup.os.tools.sh
 setup.zsh.tools.yml
-Taskfile.yml
 update-context.sh
 MRK_XRGS
 
