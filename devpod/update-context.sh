7a9465533af312851db72fa85a9239edfcc29d7f
yairdardev <yairvdar@gmail.com>
2022-04-10 17:07:05 +0300

    read

--- devpod/update-context.sh
+++ devpod/update-context.sh
@@ -1,6 +1,6 @@
 #/usr/bin/env bash
 # require 'realpath tools.shtool'
-_ME_PARENT=$(dirname $(readlink -f $0))
+_ME_PARENT=$(dirname $(realpath $0))
 
 # install/update devpod dir
 export DEVPOD_INIT_DIR="$_ME_PARENT"
